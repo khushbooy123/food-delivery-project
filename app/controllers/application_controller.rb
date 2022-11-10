f3b94dbeae6f885065c7056f4792f9ab5c8f9bdc
khushbooy123 <khushboo@webkorps.com>
2022-11-10 19:17:46 +0530

    Implemented Active admin

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,17 +1,23 @@
 class ApplicationController < ActionController::Base
-    before_action :authenticate_user!
+    # before_action :authenticate_user!
     before_action :configure_permitted_parameters, if: :devise_controller?
   
+    # def after_sign_in_path_for(resource)
+    #   if current_user.type == 'Customer'
+    #     customer_index_path
+    #   elsif current_user.type == 'Owner'
+    #     owner_index_path
+    #   else  
+    #     root_path
+    #   end
+    # end
     def after_sign_in_path_for(resource)
-      if current_user.type == 'Customer'
-        customer_index_path
-      elsif current_user.type == 'Owner'
-        owner_index_path
-      else  
-        root_path
+      if resource.is_admin?
+        redirect_to admin_path
+      else
+        redirect_to user_path
       end
     end
-
    
    def configure_permitted_parameters
     devise_parameter_sanitizer.permit(:sign_up) do |user|
--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,13 @@
 Rails.application.routes.draw do
+  devise_for :admin_users, ActiveAdmin::Devise.config
+  ActiveAdmin.routes(self)
+  
   resources :shops
   get 'owner/index'
   get 'customer/index'
-  # get 'user/index'
+  get 'user/index'
   devise_for :users
   devise_scope :user do  
     get '/users/sign_out' => 'devise/sessions#destroy'     
  end
-  root "user#index"
 end
