12982aecd0f27b2186c57adbec38a3bdfd709a53
khushbooy123 <khushboo@webkorps.com>
2022-11-21 12:31:36 +0530

    add searchbar for food

--- app/controllers/foods_controller.rb
+++ app/controllers/foods_controller.rb
@@ -1,5 +1,15 @@
 class FoodsController < InheritedResources::Base
 
+ def search
+   @food = Food.where("name LIKE ?","%" + params[:q]+ "%" )
+    # if params[:search].blank?
+    # redirect_to foods_path and return
+    # else 
+    #   @perameter = params[:search].downcase
+    #   @results = Category.all.where("lower(name) LIKE :search", search: "%#{@perameter}%")
+    # end
+ end
+
   private
 
     def food_params
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   resources :foods
+  get '/search', to:'foods#search'
   devise_for :admin_users, ActiveAdmin::Devise.config
   ActiveAdmin.routes(self)
   
